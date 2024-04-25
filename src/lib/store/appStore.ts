d0e464e21a509e0cf03b8fdf8586915a510ad220
yura <yurii@niuitmo.ru>
2024-04-25 20:54:14 +0300

    version without work tags & buttons

--- src/lib/store/appStore.ts
+++ src/lib/store/appStore.ts
@@ -1,9 +1,9 @@
-import {create} from "zustand";
-import {devtools} from "zustand/middleware";
-import {immer} from "zustand/middleware/immer";
-import {AppState} from "../types/dto/app.dto";
-import {setCookie} from "nookies";
-import {isUndefined} from "lodash";
+// import {create} from "zustand";
+// import {devtools} from "zustand/middleware";
+// import {immer} from "zustand/middleware/immer";
+// import {AppState} from "../types/dto/app.dto";
+// import {setCookie} from "nookies";
+// import {isUndefined} from "lodash";
 
 export const useAppStore = create<AppState>()(devtools(immer((set) => {
     return ({
