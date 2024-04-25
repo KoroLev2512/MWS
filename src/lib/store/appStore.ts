b8d443dd7f1a19a092da5d795ee04535810f3a27
yura <yurii@niuitmo.ru>
2024-04-25 20:57:36 +0300

    version without work tags & buttons

--- src/lib/store/appStore.ts
+++ src/lib/store/appStore.ts
@@ -4,33 +4,33 @@
 // import {AppState} from "../types/dto/app.dto";
 // import {setCookie} from "nookies";
 // import {isUndefined} from "lodash";
-
-export const useAppStore = create<AppState>()(devtools(immer((set) => {
-    return ({
-        backendIsAvailable: null,
-        isLoading: false,
-        profilePageIsClose: true,
-        menuPageIsOpen: false,
-        notificationsVisible: true,
-        isDarkMode: false,
-        toggleProfilePage: (value) => {
-            set((state) => {
-                return ({profilePageIsClose: !isUndefined(value) ? value : !state.profilePageIsClose});
-            });
-        },
-        toggleNotifications: () => {
-            set((state) => ({toggleNotifications: !state.toggleNotifications}));
-        },
-        toggleDarkMode: (value: boolean) => {
-            setCookie(null, "theme", value ? "dark" : "light", {
-                path: "/"
-            });
-            set({isDarkMode: value});
-        },
-        toggleMenuPage: (value) => {
-            set((state) => {
-                return ({menuPageIsOpen: !isUndefined(value) ? value :  !state.menuPageIsOpen});
-            });
-        },
-    });
-})));
+//
+// export const useAppStore = create<AppState>()(devtools(immer((set) => {
+//     return ({
+//         backendIsAvailable: null,
+//         isLoading: false,
+//         profilePageIsClose: true,
+//         menuPageIsOpen: false,
+//         notificationsVisible: true,
+//         isDarkMode: false,
+//         toggleProfilePage: (value) => {
+//             set((state) => {
+//                 return ({profilePageIsClose: !isUndefined(value) ? value : !state.profilePageIsClose});
+//             });
+//         },
+//         toggleNotifications: () => {
+//             set((state) => ({toggleNotifications: !state.toggleNotifications}));
+//         },
+//         toggleDarkMode: (value: boolean) => {
+//             setCookie(null, "theme", value ? "dark" : "light", {
+//                 path: "/"
+//             });
+//             set({isDarkMode: value});
+//         },
+//         toggleMenuPage: (value) => {
+//             set((state) => {
+//                 return ({menuPageIsOpen: !isUndefined(value) ? value :  !state.menuPageIsOpen});
+//             });
+//         },
+//     });
+// })));
